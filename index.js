79cb27a85151ddbe7c4a2e7f2b083becfce890d5
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-09 01:52:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').getElementsByClassName('target')[0];
+}
+
+function increaseRankBy(n) {
+  const list = document.getElementById('app').querySelectorAll('.ranked-list');
+  for(let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+} 
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  while(node.children[0]) {
+    node = node.children[0];
+  } return node;
+}
+  
+
